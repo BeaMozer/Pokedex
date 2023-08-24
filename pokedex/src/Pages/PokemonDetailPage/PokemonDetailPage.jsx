1944a43d9197873b6cbcc9dc7bb63f39b0869897
BeaMozer <beatriz.mozer@hotmail.com>
2023-08-24 18:48:18 -0300

    Adicionando as rotas

--- pokedex/src/Pages/PokemonDetailPage/PokemonDetailPage.jsx
+++ pokedex/src/Pages/PokemonDetailPage/PokemonDetailPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Header from "../../Components/Header/Header";
 
 const PokemonDetailPage = () => {
-  return <div>PokemonDetailPage</div>;
+  return (
+    <div>
+      <Header />
+      <h1>Detalhes</h1>
+    </div>
+  );
 };
 
 export default PokemonDetailPage;
---
+++ pokedex/src/Router/Coordinator.js
@@ -0,0 +1,11 @@
+export const goToDetail = (navigate) => {
+  navigate("/pokemon-detail");
+};
+
+export const goToPokedex = (navigate) => {
+  navigate("/pokedex");
+};
+
+export const goToList = (navigate) => {
+  navigate("/");
+};
